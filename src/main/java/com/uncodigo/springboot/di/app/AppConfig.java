e388019c487eb8e4487ece556b1452e1f71b3f99
felipejoq <felipejofreq@gmail.com>
2022-12-08 18:48:06 -0300

    Generando relaciones de clases con inyeccion de dependencias y creando beans en la clase de configuración

--- src/main/java/com/uncodigo/springboot/di/app/AppConfig.java
+++ src/main/java/com/uncodigo/springboot/di/app/AppConfig.java
@@ -1,25 +1,43 @@
 package com.uncodigo.springboot.di.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import com.uncodigo.springboot.di.app.models.domain.ItemFactura;
+import com.uncodigo.springboot.di.app.models.domain.Producto;
 import com.uncodigo.springboot.di.app.models.service.IServicio;
 import com.uncodigo.springboot.di.app.models.service.MiServicio;
 import com.uncodigo.springboot.di.app.models.service.MiServicioComplejo;
 
 @Configuration
 public class AppConfig {
-	
+
 	@Bean("miServicioSimplePrincipal")
 	@Primary
 	public IServicio registrarMiServicio() {
 		return new MiServicio();
 	}
-	
+
 	@Bean("miServicioComplejo")
 	public IServicio registrarMiServicioComplejo() {
 		return new MiServicioComplejo();
+	}
+
+	@Bean("itemsFactura")
+	public List<ItemFactura> registrearItems() {
+
+		Producto producto1 = new Producto("Play Station", 500000);
+		Producto producto2 = new Producto("Cámara Sony", 350000);
+
+		ItemFactura linea1 = new ItemFactura(producto1, 3);
+		ItemFactura linea2 = new ItemFactura(producto2, 1);
+
+		return Arrays.asList(linea1, linea2);
+
 	}
 
 }
--- src/main/java/com/uncodigo/springboot/di/app/models/domain/ItemFactura.java
+++ src/main/java/com/uncodigo/springboot/di/app/models/domain/ItemFactura.java
@@ -3,6 +3,11 @@ package com.uncodigo.springboot.di.app.models.domain;
 public class ItemFactura {
 	private Producto producto;
 	private Integer cantidad;
+	
+	public ItemFactura(Producto producto, Integer cantidad) {
+		this.producto = producto;
+		this.cantidad = cantidad;
+	}
 
 	public Producto getProducto() {
 		return producto;
