7f86fc7ba79c5fb4247532c04513b79ee741dad6
felipejoq <felipejofreq@gmail.com>
2022-12-08 17:46:30 -0300

    Cierre: Tres formas de inyectar dependencias 1. Constructor, 2. Atributo, 3. Metodo set

--- src/main/java/com/uncodigo/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/uncodigo/springboot/di/app/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.uncodigo.springboot.di.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +11,9 @@ import com.uncodigo.springboot.di.app.models.service.IServicio;
 public class IndexController {
 	
 	//Anotación de Spring que sirve para inyectar un componente registrado en el proyecto. (Beans).
-	
+	@Autowired 
 	private IServicio service;
 	
-	// @Autowired <- Nueva funcionalidad. Al inyectar por Constructor se puede obviar la anotación. 
-	public IndexController(IServicio service) {
-		this.service = service;
-	}
-
 	@GetMapping({"/index", "/", ""})
 	public String index(Model model) {
 		
