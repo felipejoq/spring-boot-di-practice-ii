873e69bf70b89d261c41b95e2badcba0bd475625
felipejoq <felipejofreq@gmail.com>
2022-12-08 13:06:29 -0300

    Ejemplo de cómo sería con una clase service acoplada, sin Inyección de dependencias

--- src/main/java/com/uncodigo/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/uncodigo/springboot/di/app/controllers/IndexController.java
@@ -4,11 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.uncodigo.springboot.di.app.models.service.MiServicio;
+
 @Controller
 public class IndexController {
+	
+	private MiServicio service = new MiServicio();
 
 	@GetMapping({"/index", "/", ""})
 	public String index(Model model) {
+		
+		model.addAttribute("objeto", service.operacion());
 		
 		return "index";
 	}
---
+++ src/main/java/com/uncodigo/springboot/di/app/models/service/MiServicio.java
@@ -0,0 +1,11 @@
+package com.uncodigo.springboot.di.app.models.service;
+
+public class MiServicio {
+
+	public String operacion() {
+		return "Ejecutando algún proceso desde la clase MiServicio.";
+	}
+	
+	
+	
+}
