79808fd08b3e8cd411d7d9291e5697c9e71cba35
felipejoq <felipejofreq@gmail.com>
2022-12-09 18:11:58 -0300

    Contexto (Scope) de los componentes

--- src/main/java/com/uncodigo/springboot/di/app/models/domain/Cliente.java
+++ src/main/java/com/uncodigo/springboot/di/app/models/domain/Cliente.java
@@ -2,13 +2,15 @@ package com.uncodigo.springboot.di.app.models.domain;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Component
+@RequestScope
 public class Cliente {
 
 	@Value("${cliente.nombre}")
 	private String nombre;
-	
+
 	@Value("${cliente.apellido}")
 	private String apellido;
 
--- src/main/java/com/uncodigo/springboot/di/app/models/domain/Factura.java
+++ src/main/java/com/uncodigo/springboot/di/app/models/domain/Factura.java
@@ -1,34 +1,41 @@
 package com.uncodigo.springboot.di.app.models.domain;
 
+import java.io.Serializable;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 
 @Component
-public class Factura {
+@RequestScope
+// @SessionScope // debe implementar serializable
+// @ApplicationScope
+public class Factura implements Serializable {
+
+	private static final long serialVersionUID = -2534840783083901387L;
 
 	@Value("${factura.descripcion}")
 	private String descripcion;
-	
+
 	@Autowired
 	private Cliente cliente;
-	
+
 	@Autowired
 	@Qualifier("itemsFacturaOficina")
 	private List<ItemFactura> items;
-	
+
 	@PostConstruct
 	public void inicializar() {
 		cliente.setNombre(cliente.getNombre().concat(" ").concat("Antonio"));
 		descripcion = descripcion.concat(" del cliente: ").concat(cliente.getNombre());
 	}
-	
+
 	@PreDestroy
 	public void destruir() {
 		System.out.println("Factura Destruida: ".concat(descripcion));
